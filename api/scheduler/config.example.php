ce76ad231c6689be9ba5d1689d54c68cc687d1d8
MjrMalfunk <keithj@midwestmanagedit.com>
2026-05-20 03:50:42 -0400

    Increase homepage media visibility

--- api/scheduler/config.example.php
+++ api/scheduler/config.example.php
@@ -2,9 +2,9 @@
 return [
     'provider' => 'm365',
     'calendar_user' => 'scheduling@midwestmanagedit.com',
-    'tenant_id' => '6b2aa02f-6b5b-4fec-aa27-7878f3ff3fc7',
-    'client_id' => 'eb977399-9ada-48bc-872d-091d8c3bc644',
-    'client_secret' => '~3e8Q~Yd-rTF-cSvbi.7w_wzUS1CNd8i04T1vdAc',
+    'tenant_id' => 'YOUR_TENANT_ID',
+    'client_id' => 'YOUR_CLIENT_ID',
+    'client_secret' => 'YOUR_CLIENT_SECRET',
     'timezone' => 'America/Indiana/Indianapolis',
     'timezone_label' => 'Eastern Time',
     'graph_timezone' => 'Eastern Standard Time',
